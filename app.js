9a38076e35f742517efddd41aacd4ab219c2a5ea
jerebustos <jerembustos@gmail.com>
2020-12-08 15:58:53 -0300

    subiendo

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require ("express");
+const path = require ("path")
+
+const app = express();
+const publicPhat = path.resolve(__dirname,'./public');
+
+app.use(express.static(publicPhat))
+
+
+app.listen(3000,() => {
+    console.log("servidor corriendo en el puerto 3000")
+});
+
+
+app.get('/',(req,res) => 
+{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+})
